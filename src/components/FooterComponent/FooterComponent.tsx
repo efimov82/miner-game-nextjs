f980e08103b4df2a6a490503b6db8ad88da32748
Dan Efimov <daniil.efimov@specific-group.com>
2022-01-16 17:11:39 +0300

    Fix footer

--- src/components/FooterComponent/FooterComponent.tsx
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="page-footer font-small d-flex flex-column">
+    <footer className="page-footer font-small d-flex align-items-center flex-column">
       <div className="footer-copyright text-left p-4">© 2022 Dan Efimov</div>
     </footer>
   );
